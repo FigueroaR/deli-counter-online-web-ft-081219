136b534f31e901b8832cc3bd8ffb5ec8cefcff02
Raul Figueroa <raul.a.figueroa@hotmail.com>
2019-08-16 11:34:14 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,5 +12,5 @@ end
 
 def take_a_number(deli, name)
   deli << name
-  puts "Welcome, #{name}. You are custumer number: #{deli.length}"
+  puts "Welcome, #{name}. You are number: #{deli.length} in line."
 end
